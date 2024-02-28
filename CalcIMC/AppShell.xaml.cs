f4a8626f223938860b2355b9c694d8dc89fbc186
S3_Sistemas <S3_Sistemas@SINA208DTTQ2>
2024-02-28 18:36:49 -0500

    Agregar archivos de proyecto.

---
+++ CalcIMC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CalcIMC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CalcIMC/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace CalcIMC
+{
+    public partial class MainPage : ContentPage
+    {
+    
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  void Button_Clicked(object sender, EventArgs e)
+        {
+            var peso = Convert.ToDecimal(EntryPeso.Text);
+            var estatura = Convert.ToDecimal(EntryEstatura.Text);
+
+            var imc = peso / (estatura * estatura);
+
+            EntryIMC.Text = imc.ToString();
+
+
+        }
+    }
+
+}
